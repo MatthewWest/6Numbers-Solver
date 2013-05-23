acfa09eba3d67695db0ea9a7be2feb5c537c46c1
Matthew West <matthewwest217@gmail.com>
2013-05-22 19:58:45 -0600

    Delete Code & Non-webpage stuff
    I kept forgetting I was in this branch, then doing work and having to
    redo it again. Therefore, I'm deleting all code in this branch not
    required for the website, and all development will take place in the
    other branch.

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to six-numbers-solver
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- test/clj/six_numbers_solver/core_test.clj
+++
@@ -1,8 +0,0 @@
-(ns six-numbers-solver.core-test
-  (:gen-class)
-  (:use clojure.test
-        six-numbers-solver.core))
-
-(deftest core-to-cmd-1
-  (testing "When -main is called with a -c flag, it should pass to the cmd interface"
-    (is (= "1 + 3 = 4\n4 * 2 = 8" (with-out-str (-main '-c "[8 [1 2 3]]"))))))
